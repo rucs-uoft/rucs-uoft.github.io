e857f23077e9e1f2ffba843182d709b9c94f7818
Muhammad Osama <muhammad.osama.02@gmail.com>
2015-11-06 19:40:31 -0500

    Completed 2014 edition.
    The completed design of the Fall 2014 edition of RUCS.

---
+++ js/index.js
@@ -0,0 +1,51 @@
+(function($) {
+	function scrollToContents() {
+		$('html, body').animate({
+			scrollTop: $("#table_of_contents").offset().top
+		}, 500);
+	}
+
+	$(function() {
+		$("#enter-button").click(function() {
+			scrollToContents();
+		});
+
+	$(".collection-item").click(function(e) {
+		element_id = $(this).attr('href');
+		$(".collection").children().removeClass('chapter-active');
+		$(".collection").children().removeClass('disabled');
+		$(this).addClass('chapter-active');
+		$(this).addClass('disabled');
+		$(".toc-content").children().hide();
+		$(element_id).show();
+		e.preventDefault();
+	});
+
+	$(".card").hover(
+		function() {
+			$(this).addClass('z-depth-2');
+			$(this).animate({
+				backgroundColor: "#00838f"
+			}, 250);
+		}, function() {
+			$(this).removeClass('z-depth-2');
+			$(this).animate({
+				backgroundColor: "#0097a7"
+			}, 250);
+		}
+	);
+
+	$(window).resize(function() {
+		if ($("#main-background").height() < 650) {
+			$("#down-arrow-row").hide();
+		} else {
+			$("#down-arrow-row").show();
+		}	
+	});
+	// 	var options = [
+	// 		{selector: '#index-banner', offset: 0,
+	// 		callback: "scrollToContents()"}
+	// 	];
+	// 	Materialize.scrollFire(options);
+	});
+})(jQuery);
